486d2309f3fcd651d85a44772a973051f4900035
Paolo Filipi da Costa Souza <paolocsouza@gmail.com>
2020-03-05 03:41:32 -0300

    inclusao de partials e do body parser

--- index.js
+++ index.js
@@ -1,17 +1,29 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser"); 
 
 //estou dizendo paro o express que o motor de html é o ejs
 app.set('view engine','ejs');
 app.use(express.static('public'))//para utilizar arquivos staticos
 
+//Body Parser
+app.use(bodyParser.urlencoded({extended:false}))// esse trecho que permite a tradução(vai decodificar)
+app.use(bodyParser.json());// permite que a gente leia dados de formulario enviados via json
+
+//Rotas
 app.get("/",(req,res) =>{
     res.render("index")
 })
+
 app.get("/perguntar",(req,res) =>{
     res.render("perguntar");
 })
 
+app.post("/salvarpergunta",(req,res) => {
+    var titulo = req.body.titulo;
+    var descricao = req.body.descricao;
+    res.send("Formulário recebido, titulo: " +titulo+" descrição: " + descricao);
+})
 
 app.listen(8080,() =>{
     console.log("App rodando...")
